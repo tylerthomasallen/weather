24acf3421fff4fc3f778f250e4351cb2f6bca12a
Tyler <tylerthomasallen@gmail.com>
2018-11-29 12:50:29 -0800

    Set up new react project

---
+++ congrats.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Congrats extends React.Component {
+
+    render() {
+        return (
+            <div>Hello, World!</div>
+        )
+    }
+}
+
+export default Congrats;
