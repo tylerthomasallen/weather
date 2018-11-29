342e3a4d87380229cd07dba333afb6b91ad92b81
Tyler <tylerthomasallen@gmail.com>
2018-11-29 13:00:18 -0800

    Frontend folder containing components

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Congrats from './congrats';
+import Congrats from './frontend/congrats';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
